93c3bd94b54ea50e72033b422e2d5410b35ab96c
natali <m-ema@yandex.ru>
2024-03-01 13:09:31 +0300

    start

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    /*Slider*/
+    $("[data-slider]").slick({
+    infinity: true, /*бесконечный*/
+    fade: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 1700,
+  });
+
+})
+  
